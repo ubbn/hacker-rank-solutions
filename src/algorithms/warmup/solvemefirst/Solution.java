c30fef6b6c9df28b6f58ae66428a07b10450ee17
ubbn <ubbn@live.com>
2015-12-24 18:41:35 +0100

    Initial commit

---
+++ src/algorithms/warmup/solvemefirst/Solution.java
@@ -0,0 +1,21 @@
+package algorithms.warmup.solvemefirst;
+
+/**
+ * Created by buyan on 12/24/15.
+ */
+import java.util.*;
+
+public class Solution {
+
+    static int solveMeFirst(int a, int b) {
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int sum = solveMeFirst(a, b);
+        System.out.println(sum);
+    }
+}
