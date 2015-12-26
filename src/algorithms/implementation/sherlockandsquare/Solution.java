f2ad5ac38cace200bc9a7d029e09d08564ad0baf
ubbn <ubbn@live.com>
2015-12-26 19:52:41 +0100

    Solution to Sherlock and Square

---
+++ src/algorithms/implementation/sherlockandsquare/Solution.java
@@ -0,0 +1,20 @@
+package algorithms.implementation.sherlockandsquare;
+
+import java.util.Scanner;
+
+/**
+ * Created by bb on 12/26/15.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        Scanner stdin = new Scanner(System.in);
+        int testCases = stdin.nextInt();
+
+        for (int i = 0; i < testCases; i++) {
+            int numStart = stdin.nextInt();
+            int numEnd = stdin.nextInt();
+
+            System.out.println((int)(Math.floor(Math.sqrt(numEnd)) - Math.ceil(Math.sqrt(numStart)) + 1));
+        }
+    }
+}
