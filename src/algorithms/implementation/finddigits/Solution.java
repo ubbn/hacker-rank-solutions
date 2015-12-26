4cd97edf1a7ce268814a3142c4764f535cfdba0d
ubbn <ubbn@live.com>
2015-12-26 01:28:41 +0100

    Solution to Find Digits

---
+++ src/algorithms/implementation/finddigits/Solution.java
@@ -0,0 +1,28 @@
+package algorithms.implementation.finddigits;
+
+import java.util.Scanner;
+
+/**
+ * Created by bb on 12/26/15.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        Scanner stdin = new Scanner(System.in);
+        int testCases = stdin.nextInt();
+        for (int i = 0; i < testCases; i++) {
+            int number = stdin.nextInt();
+
+            int temp = number;
+            int count = 0;
+            do {
+                int digit = temp % 10;
+                if (digit > 0 && number % digit == 0)
+                    count++;
+                temp = (temp - digit) / 10;
+            } while(temp > 0);
+
+            System.out.println(count);
+        }
+
+    }
+}
