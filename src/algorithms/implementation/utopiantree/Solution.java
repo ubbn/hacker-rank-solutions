a261e2b29c8c2aed990ab75a01743bd4c24edb5c
ubbn <ubbn@live.com>
2015-12-26 01:14:27 +0100

    Solution to Utopian Tree

---
+++ src/algorithms/implementation/utopiantree/Solution.java
@@ -0,0 +1,25 @@
+package algorithms.implementation.utopiantree;
+
+import java.util.Scanner;
+
+/**
+ * Created by bb on 12/26/15.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        Scanner stdin = new Scanner(System.in);
+        int testCases = stdin.nextInt();
+
+        for (int i = 0; i < testCases; i++) {
+            int cycles = stdin.nextInt();
+            int treeLength = 1;
+            for (int j = 0; j < cycles; j++) {
+                if (j % 2 == 0)
+                    treeLength *= 2;
+                else
+                    treeLength += 1;
+            }
+            System.out.println(treeLength);
+        }
+    }
+}
