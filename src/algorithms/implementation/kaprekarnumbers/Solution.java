fa643560d7ace94656eaf8075b86a4c2333de075
ubbn <ubbn@live.com>
2016-01-02 22:04:21 +0100

    Solution to Modified Kaprekar Numbers

---
+++ src/algorithms/implementation/kaprekarnumbers/Solution.java
@@ -0,0 +1,29 @@
+package algorithms.implementation.kaprekarnumbers;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * Created by buyan on 02/01/16.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        Scanner stdin = new Scanner(System.in);
+        int p = stdin.nextInt();
+        int q = stdin.nextInt();
+        boolean isFound = false;
+
+        for (long i = p; i <= q; i++) {
+            int numberOfDigits = (int)(Math.log10(i) + 1);
+            long sqrt = i * i;
+            int tens = (int)Math.pow(10, numberOfDigits);
+            if ((sqrt / tens + sqrt % tens) == i) {
+                System.out.print(i + " ");
+                isFound = true;
+            }
+        }
+        if (!isFound)
+            System.out.println("INVALID RANGE");
+        stdin.close();
+    }
+}
