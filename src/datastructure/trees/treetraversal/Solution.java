ca2f442242ea67c4917fe363acc5c1d6006259a5
bayarbuyan <ubbn@live.com>
2016-01-23 17:17:03 +0100

    Solution to Tree Traversal

---
+++ src/datastructure/trees/treetraversal/Solution.java
@@ -0,0 +1,31 @@
+package datastructure.trees.treetraversal;
+
+/**
+ * Created by buyan on 1/23/16.
+ */
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+}
+public class Solution {
+
+    void Preorder(Node root) {
+        System.out.print(root.data + " ");
+        if (root.left != null)
+            Preorder(root.left);
+        if (root.right != null)
+            Preorder(root.right);
+    }
+
+    void Postorder(Node root) {
+        if (root.left != null)
+            Postorder(root.left);
+        if (root.right != null)
+            Postorder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+
+}
