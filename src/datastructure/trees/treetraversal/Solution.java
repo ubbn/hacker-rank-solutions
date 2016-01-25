e435dafb7ba2451d5cfa233b00a11168c4ab5020
bayarbuyan <ubbn@live.com>
2016-01-25 22:44:21 +0100

    Solution to task for priting top view of given tree

--- src/datastructure/trees/treetraversal/Solution.java
+++ src/datastructure/trees/treetraversal/Solution.java
@@ -1,5 +1,8 @@
 package datastructure.trees.treetraversal;
 
+import java.util.LinkedList;
+import java.util.Stack;
+
 /**
  * Created by buyan on 1/23/16.
  */
@@ -11,6 +14,7 @@ class Node {
 }
 public class Solution {
 
+    // Task 1
     void Preorder(Node root) {
         System.out.print(root.data + " ");
         if (root.left != null)
@@ -19,6 +23,7 @@ public class Solution {
             Preorder(root.right);
     }
 
+    // Task 2
     void Postorder(Node root) {
         if (root.left != null)
             Postorder(root.left);
@@ -27,12 +32,38 @@ public class Solution {
         System.out.print(root.data + " ");
     }
 
+    // Task 3
     void Inorder(Node root) {
         if (root.left != null)
             Inorder(root.left);
         System.out.print(root.data + " ");
         if (root.right != null)
             Inorder(root.right);
+    }
+
+    // Task 4
+    void top_view(Node root){
+        // To store left top view
+        Stack<Integer> stackForLeft = new Stack<>();
+        // To store right top view
+        LinkedList<Integer> queueForRight = new LinkedList<>();
+
+        Node tempNode = root;
+        while(tempNode!=null){
+            queueForRight.addLast(tempNode.data);
+            tempNode = tempNode.right;
+        }
+        tempNode = root.left;
+        while(tempNode!=null){
+            stackForLeft.push(tempNode.data);
+            tempNode = tempNode.left;
+        }
+
+        while(!stackForLeft.isEmpty())
+            System.out.print(stackForLeft.pop() + " ");
+
+        for(int i : queueForRight)
+            System.out.print(i + " ");
     }
 
 }
