cd7c799b20596ef9cc501d968bc958f5756f0950
bayarbuyan <ubbn@live.com>
2016-01-31 20:01:38 +0100

    Added insert and delete node of given position

--- src/datastructure/linkedlists/operations/Solution.java
+++ src/datastructure/linkedlists/operations/Solution.java
@@ -55,4 +55,56 @@ public class Solution {
         return newNode;
     }
 
+
+    /*
+      Insert Node at a given position in a linked list
+      head can be NULL
+      First element in the linked list is at position 0
+    */
+    Node InsertNth(Node head, int data, int position) {
+        Node newNode = new Node();
+        newNode.data = data;
+
+        if (head == null || position == 0){
+            newNode.next = head;
+            return newNode;
+        }
+
+        Node node = head;
+        while(position-->1){
+            node = node.next;
+        }
+
+        Node tmp = node.next;
+        node.next = newNode;
+        newNode.next = tmp;
+
+        return head;
+    }
+
+    /*
+      Delete the node at the given position and return the head node
+      A position of 0 indicates head, a position of 1 indicates one node away
+      from the head and so on. The list may become empty after you delete the node.
+    */
+    Node Delete(Node head, int position) {
+        if (head == null)
+            return null;
+
+        if (position == 0)
+            return head.next;
+
+        Node node = head;
+        while(position-->1){
+            node = node.next;
+        }
+
+        if (node.next != null)
+            node.next = node.next.next;
+
+        return head;
+    }
+
+
+
 }
