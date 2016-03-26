0e5879c1863b577c3bb35bd0d94f503327bad24c
bayarbuyan <ubbn@live.com>
2016-03-26 09:36:53 +0100

    Solutiont to Java dequeue using array

--- src/javas/collections/javadequeue/SolutionWithArray.java
+++ src/javas/collections/javadequeue/SolutionWithArray.java
@@ -6,8 +6,9 @@ import java.util.Scanner;
 
 /**
  * Created by buyan on 3/21/16.
+ * Updated by buyan on 3/26/16
+ *
  * This solution uses array with size of all possible unique elements, 10M
- * Although it passes time limit, didn't pass memory limit
  */
 public class SolutionWithArray {
     public static void main(String[] args) {
@@ -15,30 +16,30 @@ public class SolutionWithArray {
         Deque<Integer> deque = new ArrayDeque<>();
         int n = stdin.nextInt();
         int m = stdin.nextInt();
-        int maxLength = 0;
+        int maxLength = 0, current = 0;
+        Integer temp;
         int unique[] = new int[10000001];
 
         for (int i = 0; i < n; i++) {
             int num = stdin.nextInt();
-            deque.add(num);
-            if (unique[num] == 0){
-                unique[num] = 1;
-                if (deque.size() >= m){
-                    maxLength = m;
-                    break;
+            deque.addLast(num);
+
+            if (i >= m){
+                temp = deque.removeFirst();
+                if (unique[temp] > 1)
+                    --unique[temp];
+                else{
+                    unique[temp] = 0;
+                    --current;
                 }
             }
-            else{
-                if (maxLength < deque.size() - 1)
-                    maxLength = deque.size() - 1;
-                while(true){
-                    int temp = deque.removeFirst();
-                    if (temp == num)
-                        break;
-                    else
-                        unique[temp] = 0;
-                }
-            }
+
+            if (unique[num] == 0)
+                current++;
+            ++unique[num];
+
+            if (maxLength < current)
+                maxLength = current;
         }
         System.out.println(maxLength);
 
